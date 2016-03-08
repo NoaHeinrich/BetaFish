69a1c9c157f526ab520f3b2a223e206a30bd290e
Noah Heinrich <noahheinrich@gmail.com>
2016-03-08 13:48:43 -0600

    Add genre controller

--- betafish/app/controllers/ratings_controller.rb
+++ betafish/app/controllers/ratings_controller.rb
@@ -1,1 +1,20 @@
-class RatingsCon
+class RatingsController < ApplicationController
+  def new
+  end
+
+  def create
+    @rating = Rating.new
+    @reply = Reply.find(params[:reply_id])
+    if @rating.save
+      @reply.rating = @rating
+      redirect_to @reply.story
+    else
+      render :new
+    end
+  end
+
+  private
+    def rating_params
+      params.require(:rating).permit(:score)
+    end
+end
